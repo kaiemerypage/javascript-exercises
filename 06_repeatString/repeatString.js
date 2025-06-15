66278d2ca162ad253b2030499448c31ac9057e8d
Kai Page <kaiemerypage@gmail.com>
2025-06-15 11:24:28 +0100

    Complete exercise 06

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
+  if (num < 0) {
+    return "ERROR";
+  }
 
+  let returnedString = "";
+
+  for (let i = 0; i < num; i++) {
+    returnedString += string;
+  }
+
+  return returnedString;
 };
 
 // Do not edit below this line
