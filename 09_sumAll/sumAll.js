a1db91e6506ff77037088f519a0d0d9a5d1216a1
Kai Page <kaiemerypage@gmail.com>
2025-06-22 18:42:09 +0100

    Complete exercise 09

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+  if (
+    !Number.isInteger(num1) ||
+    !Number.isInteger(num2) ||
+    num1 < 0 ||
+    num2 < 0
+  ) {
+    return "ERROR";
+  }
 
+  const startNum = Math.min(num1, num2);
+  const endNum = Math.max(num1, num2);
+
+  let sum = 0;
+  for (i = startNum; i <= endNum; i++) {
+    sum += i;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
