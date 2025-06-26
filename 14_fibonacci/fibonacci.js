24359fa3884fde19142f29c0c1833564db18b6e7
Kai Page <kaiemerypage@gmail.com>
2025-06-26 20:23:53 +0100

    Complete exercise 14

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+  if (n < 0) return "OOPS";
+  if (n == 0) return 0;
+  if (n == 1) return 1;
 
+  let prevOne = 0;
+  let prevTwo = 1;
+  let total;
+
+  for (let i = 2; i <= n; i++) {
+    total = prevOne + prevTwo;
+    prevOne = prevTwo;
+    prevTwo = total;
+  }
+
+  return total;
 };
 
 // Do not edit below this line
