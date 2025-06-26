152b8cb8be1da977f02cc9cd14a62b291605b867
Kai Page <kaiemerypage@gmail.com>
2025-06-26 19:44:33 +0100

    Complete exercise 13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  string = string.replace(/\W+/g, "").toLowerCase();
+  for (let i = 0; i < string.length / 2; i++) {
+    if (string.at(i) !== string.at(-i - 1)) {
+      return false;
+    }
+  }
+  return true;
 };
 
 // Do not edit below this line
