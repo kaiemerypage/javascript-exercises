bd7847b11d6f8760908606ea927a08787c7b0336
Kai Page <kaiemerypage@gmail.com>
2025-06-26 22:04:38 +0100

    Complete exercise 16

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
+const getAge = function (person) {
+  return (person.yearOfDeath ?? new Date().getFullYear()) - person.yearOfBirth;
+};
 
+const findTheOldest = function (people) {
+  return people.reduce((oldest, current) =>
+    getAge(current) > getAge(oldest) ? current : oldest
+  );
 };
 
 // Do not edit below this line
